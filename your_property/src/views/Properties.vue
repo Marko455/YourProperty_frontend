3ca0452591d5d33ef3450de244b3631fb2b3d36a
Marko Galavić <147241279+Marko455@users.noreply.github.com>
2026-01-12 12:37:38 +0100

    Izrada početnih elemenata
    Izrada početnih elemanata sučelja, ništa konkretno jer mi backend i dalje ne radi zbog docker dynamo dba

---
+++ your_property/src/views/Properties.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const properties = ref([]);
+
+onMounted(async () => {
+  const response = await fetch("http://localhost:8000/properties");
+  properties.value = await response.json();
+});
+</script>
+
+<template>
+  <div>
+    <h1>Properties</h1>
+    <ul>
+      <li v-for="p in properties" :key="p.id">
+        {{ p.title }} – {{ p.price }} €
+      </li>
+    </ul>
+  </div>
+</template>
